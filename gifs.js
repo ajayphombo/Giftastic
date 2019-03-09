5dc275ff6993ff66c152916ee04ac98567cbaf48
ajayphombo <sagarsu@live.com>
2019-03-08 23:25:20 -0800

    functional app

--- gifs.js
+++ gifs.js
@@ -1,30 +1,84 @@
 
 var topics=[];
 
+$("#add-topics").on("click", function(event) {
+    
+        event.preventDefault();
+
+        var topic = $("#userInput").val().trim();
+        console.log(topic);
+        topics.push(topic);
+        console.log(topics);
+        renderButtons();
+        gifSearch();
+        
+
+  });
+  renderButtons();
+
 function renderButtons(){
 
     $("#buttons-view").empty();
 
         for(var i=0;i<topics.length;i++){
             var b=$("<button>");
-            b.addClass("topic");
+            console.log(b);
+            b.addClass("topicChosen");
             b.attr("data-name",topics[i]);
             b.text(topics[i]);
             $("#buttons-view").append(b);
          }
 }
 
-$("#add-topics").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
+function gifSearch(){
+                $("button").on("click", function() {
+                    var topicGif = $(this).attr("data-name");
+                    console.log(this);
 
-    // This line will grab the text from the input box
-    var topic = $("#userInput").val().trim();
-    console.log(topic);
-    topics.push(topic);
-    console.log(topics);
-    // calling renderButtons which handles the processing of our movie array
-    renderButtons();
+                    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                    topicGif + "&api_key=fTQFhu3tMcEVU2sqaVkMweJGunYG68UR&limit=10";
+
+                    $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                    })
+
+                    .then(function(response) {
+                        var results = response.data;
+                        console.log(results);
+                        for (var i = 0; i < results.length; i++) {
+                            var gifDiv = $("<div>");
+                            var rating = results[i].rating;
+                            var p = $("<p>").text("Rating: " + rating);
+                            var topicImage = $("<img>");
+                            topicImage.attr("src", results[i].images.fixed_height_still.url);
+                            topicImage.attr("data-still",results[i].images.fixed_height_still.url);
+                            topicImage.attr("data-animate",results[i].images.fixed_height.url);
+                            topicImage.attr("data-state","still");
+                            topicImage.addClass("gif");
+                            gifDiv.append(p);
+                            gifDiv.append(topicImage);
+                            $("#display").prepend(gifDiv);
+                            changeMotion();
+
+        }
+      });
   });
-  renderButtons();
+
+};
+
+function changeMotion(){
+
+    $(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
+
+};//these codes are mostly copied from the class activities. Just rearranged them to get the desired outcome.
